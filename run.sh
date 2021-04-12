827c19c8b35470e366396f40316d294e23514e20
Sualeh Asif <sualehasif996@gmail.com>
2021-04-12 12:15:32 -0400

    Small fix to run

--- run.sh
+++ run.sh
@@ -4,10 +4,12 @@ CURR_DIR=$(pwd)
 SCRIPT_DIR="$CURR_DIR/$(dirname "$0")"
 
 #preprocess the data
-python utils/blossom5_utils.py -i $CURR_DIR/data/com-dblp.ungraph.txt
+# python utils/blossom5_utils.py -i $CURR_DIR/data/com-dblp.ungraph.txt
 
 # build Blossom V
 cd $SCRIPT_DIR/blossom5
 make
+
+echo "running blossom 5 on dblp \n \n"
 ./blossom5 -e ../data/com-dblp.ungraph.txt.blossomv.preprocessed
 
