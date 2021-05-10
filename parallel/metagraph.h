fcca49ac5a1c9ce2338ecf99165f3b5b31897ecc
Sualeh Asif <sualehasif996@gmail.com>
2021-05-10 16:38:11 -0400

    added some gbbs code

---
+++ parallel/metagraph.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "gbbs/gbbs.h"
+
+namespace gbbs {
+ 
+template <class Graph>
+inline sequence<std::tuple<uintE, uintE>> MetaGraphMatching(symmetric_graph<vertex>& G) {
+
+} 
+
+
+
+}
