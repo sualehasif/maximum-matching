78417631acfa08022ca76133c689536129d7780b
Sualeh Asif <sualehasif996@gmail.com>
2021-05-12 17:43:06 -0400

    setup

--- parallel/metagraph.h
+++ parallel/metagraph.h
@@ -1,14 +1,15 @@
-#pragma once
+# ifndef METAGRAPH_H_
+# define METAGRAPH_H_
 
 #include "gbbs/gbbs.h"
 
 namespace gbbs {
  
 template <class Graph>
-inline sequence<std::tuple<uintE, uintE>> MetaGraphMatching(symmetric_graph<vertex>& G) {
-
+inline sequence<std::tuple<uintE, uintE>> MetaGraphMatching(Graph& G) {
+    return
 } 
 
-
-
 }
+
+#endif // METAGRAPH_H_
